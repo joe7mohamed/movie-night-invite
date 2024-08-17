8a04d2919e30afe9fe4be82e389f7ecafef7a8b3
Youssef Mohamed <yomoibza@gmail.com>
2024-08-17 18:03:24 +0300

    Update project version to 1.0.0 and integrate React Router
    - Bumped the project version from 0.0.0 to 1.0.0 in package-lock.json to reflect the significant updates and improvements.
    - Integrated React Router using `HashRouter` in src/main.jsx to enable routing within the application, enhancing navigation capabilities.
    - Removed unnecessary import of `React` from src/main.jsx as it is no longer required with the updated React version.

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import '@fortawesome/react-fontawesome';
+import { HashRouter as Router } from 'react-router-dom';
+
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'bootstrap/dist/js/bootstrap.bundle.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>,
 );
